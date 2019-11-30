0419c624301cf0d23c9fbf0b12b43cd968c729ab
Brooke <brookeydai@gmail.com>
2019-11-30 14:22:15 -0800

    displays given description

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -2,19 +2,14 @@ package hello;
 
 public class Greeting {
 
-    private final long id;
-    private final String content;
+    private final String description;
 
-    public Greeting(long id, String content) {
-        this.id = id;
-        this.content = content;
+
+    public Greeting(String description) {
+        this.description = description;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
+    public String getDescription() {
+        return description;
     }
 }
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,6 +2,7 @@ package hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -12,9 +13,9 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @ResponseBody
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    public Greeting greeting(@RequestParam(value="description", defaultValue="") String desc) {
+        return new Greeting(desc);
     }
 }
