4aa8a7788731b78577caf2d933ad3f83280b2c9c
Brooke <brookeydai@gmail.com>
2019-11-30 12:09:52 -0800

    stuff

---
+++ src/test/java/com/memes/memereader/MemeReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memes.memereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
